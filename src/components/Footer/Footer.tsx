81e20185c68847213642ebb1e08de24cacb7fcd0
lubhub612 <lundhakgithub@gmail.com>
2022-07-22 08:37:48 +0000

    first commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+import { ReactComponent as QuickIcon } from 'assets/images/quickIcon.svg';
+import 'components/styles/Footer.scss';
+
+const Footer: React.FC = () => {
+  const copyrightYear = new Date().getFullYear();
+
+  return (
+    <Box className='footer'>
+      <p>© {copyrightYear} Halalswap.</p>
+    </Box>
+  );
+};
+
+export default Footer;
